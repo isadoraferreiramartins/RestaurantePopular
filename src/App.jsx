62360c227372241d40e7496e1fb5437aaa1d95d6
Aluno <aluno@senai.cabb>
2025-12-16 16:55:53 -0300

    React dia 2

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
-import "./CSS/global.css";
-import "./App.jsx";
 import Header from "./componentes/header.jsx";
+import Footer from "./componentes/Footer.jsx";
 import Pageinicio from "./componentes/Pageinicio.jsx";
 import Pagevantagens from "./componentes/Pagevantagens.jsx";
 import Pagecardapio from "./componentes/Pagecardapio.jsx";
-import Pagesobre from "./componentes/Pagesobre.jsx";
-import Pagelocalizacao from "./componentes/Pagelocalizacao.jsx";
 import Pagecontato from "./componentes/Pagecontato.jsx";
-import Footer from "./componentes/Footer.jsx";
+import Pagelocalizacao from "./componentes/Pagelocalizacao.jsx";
+import Pagesobre from "./componentes/Pagesobre.jsx";
 import Planos from "./componentes/Planos.jsx";
+
+
 
 function App() {
   return (
